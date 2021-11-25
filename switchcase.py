c181c1fceabf6a5eeb843aca30f21d73e3d48640
khyber0 <91115309+khyber0@users.noreply.github.com>
2021-11-25 16:38:54 +0800

    Update switchcase.py

--- switchcase.py
+++ switchcase.py
@@ -4,7 +4,7 @@ def addition(num1, num2):
 def subtraction(num1, num2):
   num1 -= num2
   return num1
-def mul(num1, num2):
+def multiplication(num1, num2):
   num1 *= num2
   return num1
 def division(num1, num2):
@@ -14,24 +14,24 @@ def module(num1, num2):
   num1 %= num2
   return num1
 def default(num1, num2):
-  return "Incorrect day"
+  return "Неправильное действие"
 switcher = {
     1: addition,
     2: subtraction,
-    3: mul,
+    3: multiplication,
     4: division,
     5: module
 }
 def switch(operation, num1, num2):
   return switcher.get(operation, default)(num1, num2)
 print('''Вы можете выполнить операцию
-1. Дополнение
+1. Сложение
 2. Вычитание
 3. Умножение
-4. Отдел
+4. Деление
 5. Модуль ''')
 
-choice = int(input("Выберите операцию из 1,2,3,4: "))
+choice = int(input("Выберите операцию из 1,2,3,4,5: "))
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второй номер: "))
 print (switch(choice, num1, num2))
